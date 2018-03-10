82b680c3a9af581f33519c38bbb7c41d63f4c150
Xue "Mary" Yu <32146931+XueMaryYu@users.noreply.github.com>
2018-03-10 14:30:25 -0600

    Update code1.m
    Code 1.m is the code to calculate F0.

--- code1.m
+++ code1.m
@@ -1,18 +1,19 @@
-x=[1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40]
- 
+x=[1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40]; 
+
 Fff=[];
+
 for i=1:1:150
 yn=T1(:,i);
 yn=table2array(yn);
 [param]=sigm_fit2(x,yn);
-fmax=max(yn);
-Fmax=param(2);
 Fmin=param(1);
+Fmax=param(2);
 X50=param(3);
-slop=param(4);
-Fo=(fmax)*1000/(1+10^(X50*slop))
+slope=abs(param(4));
+Fo= abs((Fmax-Fmin))/(1+exp(1)^(X50*slope))
 Fff=[Fff Fo];
+Fov=Fff'
 end
 
-yt=fsigm(param,x);
+%yt=fsigm(param,x);
 
